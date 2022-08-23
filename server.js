c7e3a049fe24db4bd4f9d1d563ad540add9de4e5
Souvik Halder <souvik.halder2020@gift.edu.in>
2022-08-24 00:16:59 +0530

    all done project completed

---
+++ server.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const app=express();
+const PORT=process.env.PORT||3000;
+
+
+const http=require('http').createServer(app);
+
+
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+});
+
+
+
+http.listen(PORT,(err)=>{
+    console.log(`Listening on the port ${PORT}`);
+})
+
+//socket
+const io=require('socket.io')(http);
+io.on('connection',(socket)=>{
+    console.log('connected');
+
+    socket.on('message',(msg)=>{
+        socket.broadcast.emit('message',msg);//it will send every connection except who sends the data
+    })
+})
